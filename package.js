91732cd353a3725b170da7093110a8a1e230e93a
Chris Mather <mather.chris@gmail.com>
2013-06-12 15:02:16 -0700

    Added tests and form control.

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  summary: 'Base class for UI controls'
+  summary: 'UI control library for Meteor' 
 });
 
 Package.on_use(function (api) {
@@ -7,16 +7,17 @@ Package.on_use(function (api) {
 
   api.add_files([
     'lib/control.js',
-    'lib/form/form.js'
+    'lib/controls/form/form.js'
   ], 'client');
 });
 
 Package.on_test(function (api) {
   api.use('controls', 'client');
-  api.use(['tinytest', 'test-helpers'], 'client');
+  api.use(['templating', 'tinytest', 'test-helpers'], 'client');
 
   api.add_files([
     'lib/control-test.js',
-    'lib/form/form-test.js'
+    'lib/controls/form/form-test.html',
+    'lib/controls/form/form-test.js'
   ], 'client');
 });

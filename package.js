d00e0c0e0bc3266cae78eafe179a59af5b175edf
Chris Mather <mather.chris@gmail.com>
2013-08-15 11:37:10 -0700

    Fix to work with Meteor 0.6.5.

--- package.js
+++ package.js
@@ -1,14 +1,21 @@
 Package.describe({
-  summary: 'UI Controls for Meteor'
+  summary: 'Alpha Project: UI Controls for Meteor'
 });
 
 Package.on_use(function (api) {
-  api.use('handlebars', 'client');
+  api.use([
+    'underscore',
+    'handlebars',
+    'templating'
+  ], 'client');
 
   api.add_files([
     'lib/control.js',
     'lib/controls/form/form.js'
   ], 'client');
+
+  if (typeof api.export !== 'undefined')
+    api.export('Control', 'client');
 });
 
 Package.on_test(function (api) {
